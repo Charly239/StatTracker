80fac370f01ed65892a359d64b623ed5f56864bb
Charly239 <charlygarcia0617@gmail.com>
2019-05-20 16:29:14 -0400

    Added API code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-  import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.fortnitetracker.com/v1/profile/xb1/bbxcharly",{
+          method: 'GET',
+          withCredentials: true,
+          credentials: 'include',
+          headers: {
+            'TRN-Api-Key': 'dfb9a605-ec4b-4c65-a0a5-c4bb6e81a1c3', //it can be iPhone or your any other attribute
+          }
+        }
+    ).then(res => res.json())
+        .then(json => {
+              this.setState({
+                isLoaded: true,
+                  item: json,
+                });
+
+              });
+            }
 
 
-
-    </div>
-  );
+  render() {
+    const { isLoaded, items } = this.state;
+     if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+          <div className = "App">
+            <ul>
+              {items.map(item => (
+                  <li key={item.stats}>
+                      Name: {item.kills}
+                  </li>
+              ))}
+            </ul>
+          </div>
+      );
+    }
+  }
 }
 
 export default App;
